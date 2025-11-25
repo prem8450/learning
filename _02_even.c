48ae476255aca8b607a7baca6df0036feead10b6
prem8450 <premstudentbussiness@gmail.com>
2025-11-25 14:59:59 +0530

    Add files via upload

---
+++ _02_even.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int i = 2;
+    while(i <= 50)
+    {
+        printf(" %d" , i);
+        printf(" \n ");
+        i++;
+    }
+    return 0;
+    
+    
+}

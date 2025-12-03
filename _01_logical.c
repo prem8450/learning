5a20d461f91052e0bc2120b200f9eec7b7bbaa0f
prem8450 <premstudentbussiness@gmail.com>
2025-12-03 22:11:35 +0530

    Add files via upload

---
+++ _01_logical.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int b = 0;
+    printf("the value of a and b is %d\n" , a&&b);
+    printf("the value of a or b is %d" , a||b);
+    printf("the the value of not(b) is %d" , !b);
+    return 0;
+}

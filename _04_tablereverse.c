6fbe7f5d5a448869b04b509a5bce95d38ff0f4bf
prem8450 <premstudentbussiness@gmail.com>
2025-12-19 20:57:38 +0530

    Add files via upload

---
+++ _04_tablereverse.c
@@ -0,0 +1,17 @@
+// program to print table of 10 in reverse order 
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d" , &n);
+
+    for (int i = 10; i >= 1; i--)
+    {
+        printf("%d * %d = %d\n" , n, i, n * i);
+    }
+    return 0;
+    
+}
